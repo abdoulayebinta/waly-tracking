e0f879b5bb71175921a82d11d10f7d025ab136ee
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-04-10 19:59:39 +0000

    Extract logo component and style-components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,13 @@
-import logo from '../assets/images/logo.svg';
 import mainImg from '../assets/images/main.svg';
-import styled from 'styled-components';
+import Wrapper from '../assets/wrappers/HomePage';
+import { Logo } from '../components/Logo';
 
 const Home = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="Waly" className="logo" />
+        <Logo />
       </nav>
-
       <div className="container page">
         <div className="info">
           <h1>
@@ -28,47 +27,5 @@ const Home = () => {
     </Wrapper>
   );
 };
-
-const Wrapper = styled.main`
-  nav {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-  }
-
-  .page {
-    min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -3rem;
-  }
-
-  h1 {
-    font-weight: 700;
-    span {
-      color: var(--primary-500);
-    }
-  }
-
-  p {
-    color: var(--grey-600);
-  }
-  .main-img {
-    display: none;
-  }
-
-  @media (min-width: 992px) {
-    .page {
-      grid-template-columns: 1fr 1fr;
-      column-gap: 3rem;
-    }
-    .main-img {
-      display: block;
-    }
-  }
-`;
 
 export default Home;
