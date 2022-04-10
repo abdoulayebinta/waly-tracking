d91671bac73d6973ecccee83502eb62ca77dca97
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-04-10 13:00:35 +0000

    Add styled-component to the landing page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import logo from '../assets/images/logo.svg';
 import mainImg from '../assets/images/main.svg';
+import styled from 'styled-components';
 
 const Home = () => {
   return (
-    <main>
+    <Wrapper>
       <nav>
         <img src={logo} alt="Waly" className="logo" />
       </nav>
@@ -24,7 +25,50 @@ const Home = () => {
 
         <img src={mainImg} alt="Job tracking" className="img main-img" />
       </div>
-    </main>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
+
 export default Home;
