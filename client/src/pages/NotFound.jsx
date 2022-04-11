8af63f86ef26cf7545e41d274046e5a176ba0086
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-04-11 10:40:59 +0000

    Add logic for not found (404) pages

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,4 +1,17 @@
+import { Link } from 'react-router-dom';
+import img from '../assets/images/404-image.svg';
+import Wrapper from '../assets/wrappers/NotFoundPage';
+
 const NotFound = () => {
-  return <h1>NotFound</h1>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="not found" />
+        <h3>Oops! page not found</h3>
+        <p>We can't seem to find the page you're looking for</p>
+        <Link to="/">back home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 export default NotFound;
