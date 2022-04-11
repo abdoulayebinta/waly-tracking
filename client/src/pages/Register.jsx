8d82be101440e829766730568fa990f79c89092e
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-04-11 21:24:25 +0000

    Add logic for registration and logging

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -13,6 +13,10 @@ const initialState = {
 const Register = () => {
   const [values, setValues] = useState(initialState);
 
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
   const handleChange = (e) => console.log(e.target);
 
   const onSubmit = (e) => {
@@ -24,15 +28,18 @@ const Register = () => {
     <Wrapper className="full-page">
       <form className="form" onSubmit={onSubmit}>
         <Logo />
-        <h3>Login</h3>
+        <h3>{values.isMember ? 'Login' : 'Register'}</h3>
         {values.showAlert && <Alert />}
         {/* name input*/}
-        <FormRow
-          type="text"
-          value={values.name}
-          name="name"
-          handleChange={handleChange}
-        />
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            value={values.name}
+            name="name"
+            handleChange={handleChange}
+          />
+        )}
+
         {/* email input*/}
         <FormRow
           type="email"
@@ -50,6 +57,12 @@ const Register = () => {
         <button type="submit" className="btn btn-block">
           submit
         </button>
+        <p>
+          {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? 'Register' : 'Login'}
+          </button>
+        </p>
       </form>
     </Wrapper>
   );
