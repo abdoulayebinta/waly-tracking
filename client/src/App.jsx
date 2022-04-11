ed49c0c751e639fb6508eca5ad6d780c0d958550
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-04-11 10:00:17 +0000

    Setup routing with React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Dashboard</div>} />
+        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<h1>Error</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
