e8b5a211f14501d70826cdfdb8a4e6d7be634668
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-04-27 12:22:26 +0000

    Add middleware to handle error and not found page

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import express from 'express';
+import errorHandlerMiddleware from './middleware/error-handler.js';
+import notFoundMiddleware from './middleware/not-found.js';
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
+  throw new Error('error');
   res.send('<h1>Welcome to the server</h1>');
 });
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}...`);
