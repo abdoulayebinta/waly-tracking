a7076d23109b451e23e84aea5e0ebdd9dc2fb008
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-05-05 15:41:34 +0000

    Setup server and add connection to MongoDB Atlas

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './db/connect.js';
 
 dotenv.config();
 
+// middleware
 import errorHandlerMiddleware from './middleware/error-handler.js';
 import notFoundMiddleware from './middleware/not-found.js';
 
@@ -11,13 +13,22 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-  throw new Error('error');
-  res.send('<h1>Welcome to the server</h1>');
+  res.send('<h1>Welcome to the server!</h1>');
 });
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    // Server will start only if connection to DB is successful
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
