94f8e9eaceb313413f711ce5ad10bbf183e83e49
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-05-06 09:48:51 +0000

    Replace hardcoded HTTP status code with http-status-code package

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,9 @@
+import { StatusCodes } from 'http-status-codes';
 import User from '../models/User.js';
 
 const register = async (req, res) => {
   const user = await User.create(req.body);
-  res.status(201).json({ user });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
