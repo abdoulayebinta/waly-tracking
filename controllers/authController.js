eeacdefb4b6d03730da877e5c4d1e9d93264e583
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-05-06 09:10:42 +0000

    Register user - Initial setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,12 @@
+import User from '../models/User.js';
+
 const register = async (req, res) => {
-  res.send('register user');
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({ user });
+  } catch (error) {
+    res.status(500).json({ msg: 'there was an error' });
+  }
 };
 
 const login = async (req, res) => {
