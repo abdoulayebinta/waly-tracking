79bece115f43d77b3a5235af3c5cc6f944d293b9
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-05-07 18:04:03 +0000

    Check for empty values in auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,12 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/User.js';
 
 const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new Error('please provide all values');
+  }
+
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
