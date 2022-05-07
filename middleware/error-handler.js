19a328ddf0bcfdde5da4282c646fa04fd9e52bf4
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-05-07 17:44:15 +0000

    Add validation for missing fields when sending POST request

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,7 +6,14 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
     msg: 'Something went wrong, try again later',
   };
-  res.status(defaultError.statusCode).json({ msg: err });
+
+  if (err.name === 'ValidationError') {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',');
+  }
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMiddleware;
