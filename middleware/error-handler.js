2347cd08a1ba9ca3b0fbf25820a328b4263484fe
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-05-07 17:56:05 +0000

    Add error handle for unique field

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -13,6 +13,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
       .map((item) => item.message)
       .join(',');
   }
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
+  }
+
   res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
