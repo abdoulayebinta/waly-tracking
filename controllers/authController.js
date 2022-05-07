743b8fae0dfc4b2cfc30d31cb01d91c0e6b499fe
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-05-07 18:54:07 +0000

    Refactor errors in a separate folder and filename

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,25 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/User.js';
-
-class CustomAPIError extends Error {
-  constructor(message) {
-    super(message);
-  }
-}
-
-class BadRequestError extends CustomAPIError {
-  constructor(message) {
-    super(message);
-    this.statusCode = StatusCodes.BAD_REQUEST;
-  }
-}
-
-class NotFoundError extends CustomAPIError {
-  constructor(message) {
-    super(message);
-    this.statusCode = StatusCodes.NOT_FOUND;
-  }
-}
+import { BadRequestError } from '../errors/index.js';
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
