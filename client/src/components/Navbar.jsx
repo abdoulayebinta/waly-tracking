cc520b8ed0b1e82e5ee7077a507c2b5024ed0e39
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-05-16 21:21:25 +0000

    Structured Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,44 @@
-import { FaHome } from 'react-icons/fa';
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
 import Wrapper from '../assets/styledComponents/Navbar';
+import Logo from './Logo';
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>Navbar</h4>
-      <FaHome />
+      <div className="nav-center">
+        <button
+          type="button"
+          className="toggle-btn"
+          onClick={() => console.log('toggle sidebar')}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log('show/hide dropdown')}
+          >
+            <FaUserCircle />
+            alpha
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => console.log('logout user')}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
