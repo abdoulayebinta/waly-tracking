ca92a5c8c80f4f1158122d79e4d2386739f17460
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-05-25 20:03:57 +0000

    Wired up small side bar component

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,9 +1,30 @@
 import Wrapper from '../assets/styledComponents/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import links from '../utils/links';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo';
 
 const SmallSidebar = () => {
   return (
     <Wrapper>
-      <h4>SmallSidebar</h4>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button
+            type="button"
+            className="close-btn"
+            onClick={() => {
+              console.log('toggle sidebar');
+            }}
+          >
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">nav links</div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
