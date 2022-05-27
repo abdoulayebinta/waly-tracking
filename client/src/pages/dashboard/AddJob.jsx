b9dc70a831a7e67b7d155d85bb4e9405523ce4df
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-05-27 17:36:11 +0000

    Add Select Job Type drop down

--- client/src/pages/dashboard/AddJob.jsx
+++ client/src/pages/dashboard/AddJob.jsx
@@ -59,6 +59,25 @@ const AddJob = () => {
             handleChange={handleJobInput}
           />
           {/* job type */}
+          <div className="form-row">
+            <label htmlFor="jobType" className="form-label">
+              job type
+            </label>
+            <select
+              name="jobType"
+              value={jobType}
+              onChange={handleJobInput}
+              className="form-select"
+            >
+              {jobTypeOptions.map((itemValue, index) => {
+                return (
+                  <option key={index} value={itemValue}>
+                    {itemValue}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
           {/* job status */}
           <div className="btn-container">
             <button
