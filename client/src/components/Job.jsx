2b5f9379fc5b7d546e1d2f07d8caac379cc17a44
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-05-29 12:58:34 +0000

    Add moment.js to format date

--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -1,4 +1,13 @@
-const Job = ({ company }) => {
-  return <h5>{company}</h5>;
+import moment from 'moment';
+
+const Job = ({ company, createdAt }) => {
+  let date = moment(createdAt);
+  date = date.format('MMM Do, YYYY');
+  return (
+    <div>
+      <h5>{company}</h5>
+      <h5>{date}</h5>
+    </div>
+  );
 };
 export default Job;
