c3f99937f86a5881c20a8a138d0c124ae35d9a4c
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-06-13 16:36:34 +0000

    Implement Edit Job feature on the Frontend

--- client/src/context/reducers.js
+++ client/src/context/reducers.js
@@ -24,6 +24,9 @@ import {
   GET_JOBS_SUCCESS,
   SET_EDIT_JOB,
   DELETE_JOB_BEGIN,
+  EDIT_JOB_BEGIN,
+  EDIT_JOB_SUCCESS,
+  EDIT_JOB_ERROR,
 } from './actions';
 
 import { initialState } from './appContext';
@@ -254,6 +257,33 @@ const reducer = (state, action) => {
 
   if (action.type === DELETE_JOB_BEGIN) {
     return { ...state, isLoading: true };
+  }
+
+  if (action.type === EDIT_JOB_BEGIN) {
+    return {
+      ...state,
+      isLoading: true,
+    };
+  }
+
+  if (action.type === EDIT_JOB_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: 'success',
+      alertText: 'Job Updated!',
+    };
+  }
+
+  if (action.type === EDIT_JOB_BEGIN) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: 'danger',
+      alertText: action.payload.msg,
+    };
   }
 
   throw new Error(`no such action: ${action.type}`);
