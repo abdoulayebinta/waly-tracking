076f79b127b3f9e23a12c47d283020fbab35f53a
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-06-13 14:04:39 +0000

    Check permission for authenticated user before update

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -5,6 +5,7 @@ import {
   NotFoundError,
   UnAuthenticatedError,
 } from '../errors/index.js';
+import checkPermissions from '../utils/checkPermissions.js';
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -40,6 +41,8 @@ const updateJob = async (req, res) => {
   if (!job) {
     throw new NotFoundError(`No job with id: ${jobId}`);
   }
+
+  checkPermissions(req.user, job.createdBy);
 
   const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
     new: true,
